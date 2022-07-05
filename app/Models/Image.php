5a11036e3caf5e13abe09c6b08438ce842606b8e
Luis Valencia <stalinvalencia24@gmail.com>
2022-07-05 08:46:36 -0500

    Creado el proyecto

---
+++ app/Models/Image.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    // alamcena las columnas(verdes imageable)
+    protected $guarded =[];
+    use HasFactory;
+    
+
+    // RELACIÓN POLIMÓRFICA DE UNO A UNO
+    public function imageable()
+    {
+        return $this->morphTo();
+    }
+
+
+}
